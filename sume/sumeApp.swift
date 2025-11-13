e46c3606d848da9ba9e10cf1850f0aaed56514c3
Reza <Reza.Rahizar@nintex.com>
2025-11-13 17:18:54 +0800

    Initial Commit

---
+++ sume/sumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  sumeApp.swift
+//  sume
+//
+//  Created by Rezamir Rahizar on 13/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct sumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sumeTests/sumeTests.swift
@@ -0,0 +1,16 @@
+//
+//  sumeTests.swift
+//  sumeTests
+//
+//  Created by Rezamir Rahizar on 13/11/2025.
+//
+
+import Testing
+
+struct sumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
