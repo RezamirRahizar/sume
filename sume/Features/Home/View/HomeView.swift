d35d804837e963556407aa4e81ca8ef70cd53bab
Reza <Reza.Rahizar@nintex.com>
2025-12-08 15:15:15 +0800

    Draft form sheet UI

--- sume/Features/Home/View/HomeView.swift
+++ sume/Features/Home/View/HomeView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var selectedDetent: PresentationDetent = .fraction(0.2)
+    
     //TODO: Move to ViewModel
     struct Output {
         var goToEdit:() -> Void
@@ -28,7 +30,7 @@ struct HomeView: View {
                 }
             ).padding(.bottom, 16)
             
-            
+        
             Button(
                 action: {
                     self.output.goToHistory()
@@ -37,7 +39,38 @@ struct HomeView: View {
                     Text("History page")
                 }
             )
+            
+        }.sheet(isPresented: .constant(true)) {
+            FormView(resumeDetails: getMockDetails())
+                .presentationDetents([.fraction(0.2), .large], selection: $selectedDetent)
+                .interactiveDismissDisabled()
+                .presentationBackgroundInteraction(
+                    .enabled(upThrough: .fraction(0.2))
+                )
+
         }
+    }
+    
+    private func getMockDetails() -> Resume {
+        let mockPersonal = PersonalDeets(firstName: "Joe", lastName:"Doe", email: "joe@email.com", address: Location(state: "Selangor", country: "Malaysia"), nationality: "Korean")
+        
+        let mockHistory = [
+            WorkExperience(companyName: "Grab", location: Location(state: "Kuala Lumpur", country: "Malaysia"), position: "Associate Quality Engineer", startDate: Date(timeIntervalSinceNow: 167000), endDate: nil, responsibilities: ["Worked on iOS projects", "Delivered results"]),
+            WorkExperience(companyName: "Foodpanda", location: Location(state: nil, country: "Malaysia"), position: "Senior Quality Engineer", startDate: Date(timeIntervalSinceNow: 167000), endDate: nil, responsibilities: ["Worked on iOS projects", "Delivered results"]),
+        ]
+        
+        let mockEducation = [
+            Education(name: "Computer Science (Software Engineering)", level: .bachelor, institution: "UiTM Tapah", location: Location(state: "Kuala Lumpur", country: "Malaysia"), startDate: Date(timeIntervalSince1970: 167000), endDate: Date.now),
+            Education(name: "Computer Science (Software Engineering)", level: .master, institution: "Harvard", location: Location(state: "Kuala Selangor", country: "Malaysia"), startDate: Date(timeIntervalSince1970: 167000), endDate: Date.now),
+        ]
+        
+        let mockSkills = [
+            Skills(type: .technical, skills: ["Coding"]),
+            Skills(type: .soft, skills: ["Speaking"])
+        ]
+        
+        return Resume(personalDetails: mockPersonal, workHistory: mockHistory, education: mockEducation, skills: mockSkills)
+        
     }
 }
 
