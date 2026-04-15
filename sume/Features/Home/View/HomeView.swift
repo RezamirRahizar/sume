468f62db1fe532a1454eaae8a071670bc393339b
Llaman <rrezamir@gmail.com>
2026-04-15 22:29:26 +0800

    Remove redundant mocker method

--- sume/Features/Home/View/HomeView.swift
+++ sume/Features/Home/View/HomeView.swift
@@ -33,7 +33,7 @@ struct HomeView: View {
                 .padding(.top, -150)
         }.sheet(isPresented: .constant(true)) {
             FormView(
-                resumeDetails: getMockDetails(),
+                resumeDetails: Mocker.getResume(),
                 isPersonalToggled: $isPersonalToggled,
                 isWorkToggled: $isWorkToggled,
                 isEducationToggled: $isEducationToggled,
@@ -62,27 +62,6 @@ struct HomeView: View {
         isExpanded = isShowingLarge
     }
     
-    private func getMockDetails() -> Resume {
-        let mockPersonal = PersonalDeets(firstName: "Joe", lastName:"Doe", email: "joe@email.com", address: Location(state: "Selangor", country: "Malaysia"), nationality: "Korean")
-        
-        let mockHistory = [
-            WorkExperience(companyName: "Grab", location: Location(state: "Kuala Lumpur", country: "Malaysia"), position: "Associate Quality Engineer", startDate: Date(timeIntervalSinceNow: 167000), endDate: nil, responsibilities: ["Worked on iOS projects", "Delivered results"]),
-            WorkExperience(companyName: "Foodpanda", location: Location(state: nil, country: "Malaysia"), position: "Senior Quality Engineer", startDate: Date(timeIntervalSinceNow: 167000), endDate: nil, responsibilities: ["Worked on iOS projects", "Delivered results"]),
-        ]
-        
-        let mockEducation = [
-            Education(name: "Computer Science (Software Engineering)", level: .bachelor, institution: "UiTM Tapah", location: Location(state: "Kuala Lumpur", country: "Malaysia"), startDate: Date(timeIntervalSince1970: 167000), endDate: Date.now),
-            Education(name: "Computer Science (Software Engineering)", level: .master, institution: "Harvard", location: Location(state: "Kuala Selangor", country: "Malaysia"), startDate: Date(timeIntervalSince1970: 167000), endDate: Date.now),
-        ]
-        
-        let mockSkills = [
-            Skills(type: .technical, skills: ["Coding"]),
-            Skills(type: .soft, skills: ["Speaking"])
-        ]
-        
-        return Resume(personalDetails: mockPersonal, workHistory: mockHistory, education: mockEducation, skills: mockSkills, lastUpdated: Date())
-        
-    }
 }
 
 #Preview {
